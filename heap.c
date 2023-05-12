3d6d40ba30f72fa05a2545cedab6846fc47cf2fd
Replit user <>
2023-05-12 18:56:42 +0000

    new try

--- heap.c
+++ heap.c
@@ -63,29 +63,34 @@ pq->size++;
 
 
 void heap_pop(Heap* pq){
-
+  //compruebo de que el heap no este vacio
+  if (pq->size <= 0) {
+        return; // el heap está vacío
+  }
   //intercambio valores de la raiz y de el ultimo dato
   pq->heapArray[0]=pq->heapArray[pq->size];
   pq->size--;
   //a continuacion reordenare los datos para mantener el monticulo
-  //definiendo valores para la funcion siguiente
   heapElem aux;
-  //representa al hijo
-  int s=pq->size;
-  //representa al padre
-  int padre=(s-1)/2;
-  while((pq->heapArray[s].priority)>(pq->heapArray[padre].priority)){
-    if((pq->heapArray[s].priority)>(pq->heapArray[padre].priority)){ 
-      aux=pq->heapArray[s];
-      pq->heapArray[s]=pq->heapArray[padre];
-      pq->heapArray[padre]=aux;
-      s=padre;
-      padre=(s-1)/2;
+  for(int i=0;i<pq->capac;i++){
+    //defino los valores hijos
+    int left =2i+1;
+    int right=2i+2;
+    int posPadre=(i-1)/2;
+    //en caso de que el hijo izquierdo sea mayor que el izquierdo este se intercambiara con el padre y lo mismo en caso contrario
+    if(pq->heapArray[left].priority<pq->heapArray[right].priority){
+      aux=pq->heapArray[left];
+      pq->heapArray[left]=pq->heapArray[posPadre];
+      pq->heapArray[posPadre]=aux;
     }
-    else break;
+    else{
+      if(pq->heapArray[right].priority<pq->heapArray[left].priority){
+      aux=pq->heapArray[right];
+      pq->heapArray[right]=pq->heapArray[posPadre];
+      pq->heapArray[posPadre]=aux;
+      }
+    }
   }
-  //actualizo el size
-  pq->size++;
 }
 
 Heap* createHeap(){
